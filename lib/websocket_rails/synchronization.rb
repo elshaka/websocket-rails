f849e8a6c7ec5b45bba89226034f23e4847790ca
Eleazar Meza <meza.eleazar@gmail.com>
2014-04-18 01:30:54 -0430

    Always use synchrony driver for redis synchronization client

--- lib/websocket_rails/synchronization.rb
+++ lib/websocket_rails/synchronization.rb
@@ -74,7 +74,8 @@ module WebsocketRails
         register_server(@server_token)
 
         synchro = Fiber.new do
-          fiber_redis = Redis.connect(WebsocketRails.config.redis_options)
+          redis_options = WebsocketRails.config.redis_options.merge(:driver => :synchrony)
+          fiber_redis = Redis.connect(redis_options)
           fiber_redis.subscribe "websocket_rails.events" do |on|
 
             on.message do |_, encoded_event|
